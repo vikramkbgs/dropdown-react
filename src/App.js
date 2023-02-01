40af07f2ecaa0286867cc7a718e376ec9cd70f2f
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-02-01 15:08:30 +0530

    third commit: dropdown completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Dropdown from "./Dropdown";
 function App() {
   return (
     <div className="flex flex-col h-screen items-center justify-center bg-gray-300">
-      <div className="w-96 h-auto bg-red-600 p-6 rounded-lg shadow-lg">
-        <h3>Should you use a dropdown?</h3>
+      <div className="w-96 h-72 bg-purple-100 p-6 rounded-lg shadow-lg">
+        <h3 className="text-base font-bold text-purple-900">Should you use a dropdown?</h3>
         <Dropdown />
       </div>
     </div>
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,57 +1,60 @@
 import React, { useState } from "react";
+import { AiFillCaretDown } from "react-icons/ai";
+
+function DropdownButton(props) {
+  return (
+    <button
+      className="text-left rounded w-52 h-12 mt-2  p-3 flex justify-between bg-sky-300 hover:bg-sky-500"
+      onClick={props.onClick}
+      onMouseOver={props.onMouseOver}
+    >
+      {props.selectedOption}
+      <AiFillCaretDown className="mt-1" />
+    </button>
+  );
+}
+
+function OptionsList(props) {
+  return (
+    <div className="mt-2">
+      {props.options.map((option) => (
+        <div
+          key={option.value}
+          className="p-4 bg-sky-100 shadow-md hover:bg-slate-400 cursor-pointer"
+          onClick={() => {
+            props.setSelectedOption(option.value);
+            props.setShowOptions(false);
+          }}
+        >
+          {option.label}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function Dropdown() {
-  const [selectedOption, setSelectedOption] = useState("Option 1");
-  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("Select");
+  const [showOptions, setShowOptions] = useState(false);
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
-    setIsOpen(false);
-  };
+  const options = [
+    { value: "Yes", label: "Yes" },
+    { value: "Probably Not", label: "Probably Not" },
+  ];
 
   return (
-    <div className="relative">
-      <div className="flex w-20">
-        <button className="rounded bg-white" onClick={handleToggle}>
-          {selectedOption}‖
-        </button>
-      </div>
-      {isOpen && (
-        <div className="absolute z-10 bg-white shadow-md rounded mt-4">
-          <ul className="list-reset">
-            <li>
-              <button
-                className="block w-full px-4 py-2 text-left"
-                value="Option 1"
-                onClick={handleChange}
-              >
-                Option 1
-              </button>
-            </li>
-            <li>
-              <button
-                className="block w-full px-4 py-2 text-left"
-                value="Option 2"
-                onClick={handleChange}
-              >
-                Option 2
-              </button>
-            </li>
-            <li>
-              <button
-                className="block w-full px-4 py-2 text-left"
-                value="Option 3"
-                onClick={handleChange}
-              >
-                Option 3
-              </button>
-            </li>
-          </ul>
-        </div>
+    <div>
+      <DropdownButton
+        selectedOption={selectedOption}
+        onClick={() => setShowOptions(!showOptions)}
+        onMouseOver={() => setShowOptions(true)}
+      />
+      {showOptions && (
+        <OptionsList
+          options={options}
+          setSelectedOption={setSelectedOption}
+          setShowOptions={setShowOptions}
+        />
       )}
     </div>
   );
